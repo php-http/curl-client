10d80303dc38cc2fe99f03f875a4c4f28f9e9d72
Михаил Красильников <m.krasilnikov@yandex.ru>
2016-06-14 10:37:01 +0300

    Workaround for https://github.com/puli/issues/issues/190

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+$loader = require __DIR__ . '/../vendor/autoload.php';
+
+// Temporary fix for Puli
+$loader->addClassMap(
+    [
+        'Puli\\GeneratedPuliFactory' => __DIR__ . '/../.puli/GeneratedPuliFactory.php'
+    ]
+);
