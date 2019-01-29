4c7769f268f652e62e7466d8cd3c785081e35ee1
Михаил Красильников <m.krasilnikov@yandex.ru>
2019-01-29 17:10:58 +0300

    tests: ClientTest formatted and rearranged

--- tests/Unit/ClientTest.php
+++ tests/Unit/ClientTest.php
@@ -17,11 +17,26 @@ use Zend\Diactoros\Request;
 class ClientTest extends TestCase
 {
     /**
-     * "Expect" header should be empty.
+     * Tests throwing InvalidArgumentException when invalid cURL options passed to constructor.
+     */
+    public function testExceptionThrownOnInvalidCurlOptions(): void
+    {
+        $this->expectException(InvalidOptionsException::class);
+        new Client(
+            $this->createMock(ResponseFactoryInterface::class),
+            $this->createMock(StreamFactoryInterface::class),
+            [
+                CURLOPT_HEADER => true, // this won't work with our client
+            ]
+        );
+    }
+
+    /**
+     * "Expect" header should be empty by default.
      *
      * @link https://github.com/php-http/curl-client/issues/18
      */
-    public function testExpectHeader(): void
+    public function testExpectHeaderIsEmpty(): void
     {
         $client = $this->createMock(Client::class);
 
@@ -36,11 +51,11 @@ class ClientTest extends TestCase
     }
 
     /**
-     * "Expect" header should be empty.
+     * "Expect" header should be empty when POST field is empty.
      *
      * @link https://github.com/php-http/curl-client/issues/18
      */
-    public function testWithNullPostFields(): void
+    public function testExpectHeaderIsEmpty2(): void
     {
         $client = $this->createMock(Client::class);
 
@@ -53,21 +68,6 @@ class ClientTest extends TestCase
         $headers = $createHeaders->invoke($client, $request, [CURLOPT_POSTFIELDS => '']);
 
         self::assertContains('content-length: 0', $headers);
-    }
-
-    public function testRewindStream(): void
-    {
-        $client = $this->createMock(Client::class);
-
-        $bodyOptions = new \ReflectionMethod(Client::class, 'addRequestBodyOptions');
-        $bodyOptions->setAccessible(true);
-
-        $body = \GuzzleHttp\Psr7\stream_for('abcdef');
-        $body->seek(3);
-        $request = new Request('http://foo.com', 'POST', $body);
-        $options = $bodyOptions->invoke($client, $request, []);
-
-        static::assertEquals('abcdef', $options[CURLOPT_POSTFIELDS]);
     }
 
     public function testRewindLargeStream(): void
@@ -88,21 +88,24 @@ class ClientTest extends TestCase
         $request = new Request('http://foo.com', 'POST', $body);
         $options = $bodyOptions->invoke($client, $request, []);
 
-        static::assertTrue(false !== strstr($options[CURLOPT_READFUNCTION](null, null, $length), 'abcdef'), 'Steam was not rewinded');
+        static::assertTrue(
+            false !== strstr($options[CURLOPT_READFUNCTION](null, null, $length), 'abcdef'),
+            'Steam was not rewinded'
+        );
     }
 
-    /**
-     * Tests throwing InvalidArgumentException when invalid cURL options passed to constructor.
-     */
-    public function testInvalidCurlOptions(): void
+    public function testRewindStream(): void
     {
-        $this->expectException(InvalidOptionsException::class);
-        new Client(
-            $this->createMock(ResponseFactoryInterface::class),
-            $this->createMock(StreamFactoryInterface::class),
-            [
-                CURLOPT_HEADER => true, // this won't work with our client
-            ]
-        );
+        $client = $this->createMock(Client::class);
+
+        $bodyOptions = new \ReflectionMethod(Client::class, 'addRequestBodyOptions');
+        $bodyOptions->setAccessible(true);
+
+        $body = \GuzzleHttp\Psr7\stream_for('abcdef');
+        $body->seek(3);
+        $request = new Request('http://foo.com', 'POST', $body);
+        $options = $bodyOptions->invoke($client, $request, []);
+
+        static::assertEquals('abcdef', $options[CURLOPT_POSTFIELDS]);
     }
 }
